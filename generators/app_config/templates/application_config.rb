d4bc5f5b19c8aaeadde1ae61c444f1cdb467ab9e
fistfvck <fistfvck@gmail.com>
2010-05-06 16:28:48 +0900

    compat rails 2.3

--- generators/app_config/templates/application_config.rb
+++ generators/app_config/templates/application_config.rb
@@ -1,3 +1,3 @@
-APP_CONFIG = YAML.load(File.read(RAILS_ROOT + "/config/application.yml"))[RAILS_ENV]
+APP_CONFIG = YAML.load_file(Rails.root + "config/application.yml")[Rails.env]
 # This will let you more easily use helpers based on url_for in your mailers.
 ActionMailer::Base.default_url_options[:host] = APP_CONFIG['host']
