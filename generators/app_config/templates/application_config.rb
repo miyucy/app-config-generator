bb33194c504dd09721af390b380c9f5a16762113
fistfvck <fistfvck@gmail.com>
2010-05-18 01:19:12 +0900

    symbolize

--- generators/app_config/templates/application_config.rb
+++ generators/app_config/templates/application_config.rb
@@ -1,3 +1,3 @@
-APP_CONFIG = YAML.load_file(Rails.root + "config/application.yml")[Rails.env]
+APP_CONFIG = YAML.load_file(Rails.root + "config/application.yml")[Rails.env].symbolize_keys
 # This will let you more easily use helpers based on url_for in your mailers.
-ActionMailer::Base.default_url_options[:host] = APP_CONFIG['host']
+ActionMailer::Base.default_url_options[:host] = APP_CONFIG[:host]
